d2e6352ffa359849d7438ba0ed56b22faa52a51d
Artsiom Tsaryonau <Artsiom_Tsaryonau@epam.com>
2018-10-15 16:03:06 +0300

    Minor refactoring

--- src/main/java/com/epam/project/Main.java
+++ src/main/java/com/epam/project/Main.java
@@ -12,27 +12,23 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Main {
 
-    private static final Injector INJECTOR = Guice.createInjector(new Neo4jOGM());
-
-    private static <T> T require(Class<T> clazz) {
-        return INJECTOR.getInstance(clazz);
-    }
-
     public static void main(String[] args) {
-        var app = Javalin.create().start(8888);
+        Injector injector = Guice.createInjector(new Neo4jOGM());
+        var app = Javalin.create();
         app.routes(() -> {
            path("movies", () -> {
                get(ctx -> {
-                    var session = require(SessionFactory.class).openSession();
+                    var session = injector.getInstance(SessionFactory.class).openSession();
                     ctx.json(session.loadAll(Movie.class));
                });
 
                get(":id", ctx -> {
-                    var session = require(SessionFactory.class).openSession();
+                    var session = injector.getInstance(SessionFactory.class).openSession();
                     long value = Long.valueOf(ctx.pathParam("id"));
                     ctx.json(session.load(Movie.class, value));
                });
            });
         });
+        app.start(8888);
     }
 }
